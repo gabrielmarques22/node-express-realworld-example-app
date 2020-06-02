7ca68b7696a848b9923733df45163ef2e498be6a
GabrielMarques <gabrielfelipemarques@live.com>
2020-06-02 18:48:32 -0300

    Dynatrace Integration

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -9,6 +9,6 @@ router.get('/', function(req, res, next) {
   }).catch(next);
 });
 router.get('/teste', function(req, res, next){
-	return res.json({responseCode: 200, message: "Teste Update 2"});
+	return res.json({responseCode: 200, message: "Teste Update Dyna Integration"});
 });
 module.exports = router;
