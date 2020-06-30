0d2a2956381c551d16cd4b611657a147a90141e8
GabrielMarques <gabrielfelipemarques@live.com>
2020-06-30 11:33:18 -0300

    Fix App

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -63,7 +63,7 @@ router.get('/', auth.optional, function(req, res, next) {
     }
 
     return Promise.all([
-      Article.find(superUndefinedVariable)
+      Article.find(query)
         .limit(Number(limit))
         .skip(Number(offset))
         .sort({createdAt: 'desc'})
